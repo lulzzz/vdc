df0f3031c3b4b697fff20a56970e2a57334f7409
kungumaraj nachimuthu <senthilkungumaraj@gmail.com>
2019-08-26 21:03:52 -0700

    Merged the rootcert.ps1 and rootcert.sh

--- Modules/VirtualNetworkGateway/2.0/Scripts/virtual.network.gateway.rootcert.ps1
+++ Modules/VirtualNetworkGateway/2.0/Scripts/virtual.network.gateway.rootcert.ps1
@@ -16,10 +16,11 @@ param(
 
 Import-Module -Name Az
 
-if($Env:OS -like "*windows*" -or $IsWindows -eq $true) {
-    $keyExists = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName
+$keyExists = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName -ErrorAction SilentlyContinue;
 
-    if($null -ne $keyExists) {
+if($null -eq $keyExists) {
+
+    if($Env:OS -like "*windows*" -or $IsWindows -eq $true) {
         Write-Host "Generating Root Cert for Windows";
         $certPath = "Cert:\CurrentUser\My";
         $rootCert = Get-ChildItem -Path $certPath | Where-Object { $_.Subject -eq "CN=VPN CA" };
@@ -29,16 +30,19 @@ if($Env:OS -like "*windows*" -or $IsWindows -eq $true) {
                     -HashAlgorithm sha256 -KeyLength 2048 `
                     -CertStoreLocation $certPath -KeyUsageProperty Sign -KeyUsage CertSign;
         }
+        
         $rootCertPublicKey = $rootCert.GetPublicKeyString();
         Export-Certificate -Cert $rootCert.PSPath -FilePath C:\certs\rootCert.cer
         $rootCertPublicKey = $rootCert.GetRawCertDataString();
         $rootCertPublicKey = [Convert]::ToBase64String($rootCertPublicKey);
-        $secureString = ConvertTo-SecureString -String $rootCertPublicKey -AsPlainText -Force;
-        Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName -SecretValue $secureString;
     }
-}
-else {
-    Write-Host "Generating Root Cert for Linux";
-    Get-Location | Write-Host;
-    bash -c "$BashScriptPath $TenantId $ServicePrincipal_ID $ServicePrincipal_Secret $KeyVaultName $KeyName";
+    else {
+        Write-Host "Generating Root Cert for Linux";
+        Get-Location | Write-Host;
+        bash -c "chmod 755 $BashScriptPath"
+        $rootCertPublicKey = (bash -c "$BashScriptPath $TenantId $ServicePrincipal_ID $ServicePrincipal_Secret $KeyVaultName $KeyName")[0];
+    }
+    Write-Host $rootCertPublicKey;
+    $secureString = ConvertTo-SecureString -String $rootCertPublicKey -AsPlainText -Force;
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName -SecretValue $secureString;
 }
