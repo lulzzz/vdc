243db05c1d74653e720265923feec91469d79f66
Kungumaraj Nachimuthu <senthilkungumaraj@gmail.com>
2019-08-28 22:24:02 +0000

    Fix for p2s script for Windows VM

--- Modules/VirtualNetworkGateway/2.0/Scripts/virtual.network.gateway.rootcert.ps1
+++ Modules/VirtualNetworkGateway/2.0/Scripts/virtual.network.gateway.rootcert.ps1
@@ -19,21 +19,32 @@ Import-Module -Name Az
 if($Env:OS -like "*windows*" -or $IsWindows -eq $true) {
     $keyExists = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName
 
-    if($null -ne $keyExists) {
+    if($null -eq $keyExists) {
         Write-Host "Generating Root Cert for Windows";
-        $certPath = "Cert:\CurrentUser\My";
-        $rootCert = Get-ChildItem -Path $certPath | Where-Object { $_.Subject -eq "CN=VPN CA" };
-        if($null -eq $rootCert) {
-            $rootCert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
-                    -Subject "CN=VPN CA" -KeyExportPolicy Exportable `
-                    -HashAlgorithm sha256 -KeyLength 2048 `
-                    -CertStoreLocation $certPath -KeyUsageProperty Sign -KeyUsage CertSign;
+        # Run the script in PowerShell 5 or less. If you're running in PowerShell Core,
+        # New-SelfSignedCertificate is not available. So, make sure your windows machine
+        # has both PowerShell Core and PowerShell.
+        $secureString = Powershell -Command {
+            New-Item -Path "C:\" -Name "certs" -ItemType "directory" | Out-Null;
+            $certFilePath = "C:\certs\rootCert.cer";
+            $certPath = "Cert:\CurrentUser\My";
+            $rootCert = Get-ChildItem -Path $certPath | Where-Object { $_.Subject -eq "CN=VPN CA" };
+            if($null -eq $rootCert) {
+                $rootCert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+                        -Subject "CN=VPN CA" -KeyExportPolicy Exportable `
+                        -HashAlgorithm sha256 -KeyLength 2048 `
+                        -CertStoreLocation $certPath -KeyUsageProperty Sign -KeyUsage CertSign;
+            }
+            Export-Certificate -Cert $rootCert.PSPath -FilePath $certFilePath | Out-Null;
+            $rootCertPublicKey = $rootCert.GetRawCertData();
+            $rootCertPublicKey = [Convert]::ToBase64String($rootCertPublicKey);
+            if(Test-Path $certFilePath) {
+                Remove-Item -Path $certFilePath;
+                Remove-Item -Path "C:\certs";
+            }
+            return ConvertTo-SecureString -String $rootCertPublicKey -AsPlainText -Force;
+            
         }
-        $rootCertPublicKey = $rootCert.GetPublicKeyString();
-        Export-Certificate -Cert $rootCert.PSPath -FilePath C:\certs\rootCert.cer
-        $rootCertPublicKey = $rootCert.GetRawCertDataString();
-        $rootCertPublicKey = [Convert]::ToBase64String($rootCertPublicKey);
-        $secureString = ConvertTo-SecureString -String $rootCertPublicKey -AsPlainText -Force;
         Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName -SecretValue $secureString;
     }
 }
