0bb3f8586d6053e67669eeea73f96953990b3af1
kungumaraj nachimuthu <senthilkungumaraj@gmail.com>
2019-08-29 12:20:35 -0700

    Updated the CreateRootCertificate module

--- Modules/VirtualNetworkGateway/2.0/Scripts/virtual.network.gateway.rootcert.ps1
+++ Modules/VirtualNetworkGateway/2.0/Scripts/virtual.network.gateway.rootcert.ps1
@@ -1,12 +1,6 @@
 [CmdletBinding()] 
 param(
     [Parameter(Mandatory=$true)]
-    [string]$TenantId,
-    [Parameter(Mandatory=$true)]
-    [string]$ServicePrincipal_ID,
-    [Parameter(Mandatory=$true)]
-    [string]$ServicePrincipal_Secret,
-    [Parameter(Mandatory=$true)]
     [string]$KeyVaultName,
     [Parameter(Mandatory=$true)]
     [string]$KeyName,
@@ -16,33 +10,44 @@ param(
 
 Import-Module -Name Az
 
-$keyExists = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName -ErrorAction SilentlyContinue;
+
+$keyExists = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName
 
 if($null -eq $keyExists) {
 
     if($Env:OS -like "*windows*" -or $IsWindows -eq $true) {
+        $cwd = Get-Location;
         Write-Host "Generating Root Cert for Windows";
-        $certPath = "Cert:\CurrentUser\My";
-        $rootCert = Get-ChildItem -Path $certPath | Where-Object { $_.Subject -eq "CN=VPN CA" };
-        if($null -eq $rootCert) {
-            $rootCert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
-                    -Subject "CN=VPN CA" -KeyExportPolicy Exportable `
-                    -HashAlgorithm sha256 -KeyLength 2048 `
-                    -CertStoreLocation $certPath -KeyUsageProperty Sign -KeyUsage CertSign;
+        # Run the script in PowerShell 5 or less. If you're running in PowerShell Core,
+        # New-SelfSignedCertificate is not available. So, make sure your windows machine
+        # has both PowerShell Core and PowerShell.
+        $rootCertPublicKey = Powershell -Command {
+            New-Item -Path $cwd -Name "certs" -ItemType "directory" | Out-Null;
+            $certFilePath = "$cwd\certs\rootCert.cer";
+            $certPath = "Cert:\CurrentUser\My";
+            $rootCert = Get-ChildItem -Path $certPath | Where-Object { $_.Subject -eq "CN=VPN CA" };
+            if($null -eq $rootCert) {
+                $rootCert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+                        -Subject "CN=VPN CA" -KeyExportPolicy Exportable `
+                        -HashAlgorithm sha256 -KeyLength 2048 `
+                        -CertStoreLocation $certPath -KeyUsageProperty Sign -KeyUsage CertSign;
+            }
+            Export-Certificate -Cert $rootCert.PSPath -FilePath $certFilePath | Out-Null;
+            $rootCertPublicKey = $rootCert.GetRawCertData();
+            $rootCertPublicKey = [Convert]::ToBase64String($rootCertPublicKey);
+            if(Test-Path $certFilePath) {
+                Remove-Item -Path $certFilePath;
+                Remove-Item -Path "$cwd\certs";
+            }
+            return $rootCertPublicKey;
         }
-        
-        $rootCertPublicKey = $rootCert.GetPublicKeyString();
-        Export-Certificate -Cert $rootCert.PSPath -FilePath C:\certs\rootCert.cer
-        $rootCertPublicKey = $rootCert.GetRawCertDataString();
-        $rootCertPublicKey = [Convert]::ToBase64String($rootCertPublicKey);
     }
     else {
         Write-Host "Generating Root Cert for Linux";
-        Get-Location | Write-Host;
         bash -c "chmod 755 $BashScriptPath"
-        $rootCertPublicKey = (bash -c "$BashScriptPath $TenantId $ServicePrincipal_ID $ServicePrincipal_Secret $KeyVaultName $KeyName")[0];
+        $bashScriptResult = (bash -c "$BashScriptPath $KeyVaultName $KeyName $cwd");
+        $rootCertPublicKey = $bashScriptResult[$bashScriptResult.Count - 1];
     }
-    Write-Host $rootCertPublicKey;
     $secureString = ConvertTo-SecureString -String $rootCertPublicKey -AsPlainText -Force;
     Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyName -SecretValue $secureString;
 }
